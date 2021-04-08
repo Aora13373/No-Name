462453019a54e08606a789e95f073a191490bcb2
dan Strømsøy <80658566+Aora13373@users.noreply.github.com>
2021-04-08 14:54:26 +0200

    Docstring and comment modidifikations

--- db.py
+++ db.py
@@ -2,14 +2,11 @@ from pymongo import MongoClient
 from flask import current_app, g
 
 def get_db():
-    '''Docstring'''
-        
-    # Setting the required information in order to establish the connection between db and g
+    '''Setting up connection to the databse / verifying the connection'''
+
     if 'db' not in g:
         client = MongoClient("mongodb+srv://hakon:123@studio.r7myl.mongodb.net/testing?retryWrites=true&w=majority") 
         g.db = client.FactBook    
         return g.db
-    
-    # We are certain that db is not empty and that it has been connected with g. so we simply return that connection.
     else:
         return g.db
