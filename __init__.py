0f6b21f7bc62965249b22e8572cd981c874c094c
dan Strømsøy <80658566+Aora13373@users.noreply.github.com>
2021-04-08 16:15:15 +0200

    Comments and docstring editing

--- __init__.py
+++ __init__.py
@@ -2,25 +2,19 @@
 import os
 from flask import Flask
 
-
 # Function that is used in order to create our 'app'
 def create_app(test_config=None):
-    '''Docstring'''
+    '''
+    sets the test_config=None when called.
+    creates a variable to contain our Flask application, with the required configurations
+    Configures the secret keys
+    uses an if test in order to configure and load the config file to use for our app
+    imports some required local files in order to register both blueprints and url rules
+    '''
     
     app = Flask(__name__, instance_relative_config=True)
-    # __name__ : Telling Python to use the current python module in order to configure the required paths.
-    # instance_relative_config=True : 
-        # instance    : Foldername from where to load config files.
-        # relative    : Path to the folder. 
-        # config=True : Where python should store files that will not be commited to version control etc
-
-    app.config.from_mapping(
-        SECRET_KEY='dev'
-        )
-    # Secret key is used by Flask and extensions in order to keep data safe.
-    # The 'dev' SHOULD be overriden with a random value when deploying!
-
-    # Loop to veriy that our configuration has been configured.
+    app.config.from_mapping(SECRET_KEY='dev')
+    
     if test_config is None:  
         app.config.from_pyfile('config.py', silent=True)
         # Loading the configurations from config.py
@@ -28,24 +22,17 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
         # Loading the configurations from test_config 
 
-
     # Checking if the required folders for our app exists and can be used by our app.
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    # Remember that the os module is unnable to create these required folders if they do not exist.
 
-
-
-    # Testing how to build a connection between a simple page and a function
-# ---------------------------------------
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-# ---------------------------------------   
 
-    # Import all the blueprints and define url rules
+    # Import all the blueprints and defining url rules
 # -------------------------------------------------------
     from . import index
     app.register_blueprint(index.bp)
@@ -66,5 +53,5 @@ def create_app(test_config=None):
     from . import scandinavian
     app.register_blueprint(scandinavian.bp)
     app.add_url_rule('/scandinavia', endpoint='scandinavia')
-# -------------------------------------------------------
+    
     return app
